a3e683ac9380c00d274ad7f5e4099ac057f8dcf7
Kimp13 <akunec41@gmail.com>
2020-08-22 12:02:25 +0300

    Подправил конфиг страпи под разные окружения.

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,25 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '72feaf4a1d553e8c4767a912c9872d32'),
-    },
-  },
-});
+module.exports = ({ env }) => {
+  if (env('NODE_ENV', 'production') === 'production') {
+    return {
+      host: env('HOST', '0.0.0.0'),
+      port: env.int('PORT', 1337),
+      url: 'https://villafiolent.ru/api',
+      admin: {
+        url: 'https://villafiolent.ru/dashboard',
+        auth: {
+          secret: env('ADMIN_JWT_SECRET', '72feaf4a1d553e8c4767a912c9872d32'),
+        }
+      }
+    };
+  } else {
+    return {
+      host: env('HOST', '0.0.0.0'),
+      port: env.int('PORT', 1337),
+      admin: {
+        auth: {
+          secret: env('ADMIN_JWT_SECRET', '72feaf4a1d553e8c4767a912c9872d32'),
+        }
+      }
+    };
+  }
+};
