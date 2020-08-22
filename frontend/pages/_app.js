fc245fcb8cda2740f2ef664491b04c5e6aab833e
Kimp13 <akunec41@gmail.com>
2020-08-22 16:50:23 +0300

    Сокет.иооо...

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -25,7 +25,10 @@ export default class MyApp extends App {
   }
 
   componentDidMount() {
-    const socket = io.connect(getFullLink('/'), {path: '/api/socket.io'});
+    const socket = io.connect(getFullLink('/'), {
+      path: 'api/socket.io',
+      transports: ['websocket']
+    });
     socket.on('tokenExpired', e => {
       alert(e);
     });
